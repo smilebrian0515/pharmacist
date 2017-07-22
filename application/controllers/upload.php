23fd591e9d6d7d06e93f155e55b7c9df231daa71
謹誠 <smilebrian0515@gmail.com>
2017-07-22 14:59:10 +0800

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pharmacist extends CI_Controller {
+class Upload extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -21,10 +21,7 @@ class Pharmacist extends CI_Controller {
 	public function index()
 	{
 		$data = array();
-		$this->parser->parse('pharmacist',$data);
+		$this->parser->parse('upload',$data);
 	}
-	public function blank () {
-		$data = array();
-		$this->parser->parse('blank',$data);
-	}
+
 }
